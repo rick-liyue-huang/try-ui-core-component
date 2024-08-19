1a71bea7b74343647e67aafc1f2ca32de8b1718a
Rick Huang <RickHuang@rauland.com.au>
2024-08-19 20:39:23 +1000

    try

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 import './App.css'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-// import { Button } from './components'
-import { Button } from '../dist/try-ui-core-component'
+import { Button } from './components'
+// import { Button } from '../dist/try-ui-core-component'
 import { darkTheme } from './theme'
 
 function App() {
